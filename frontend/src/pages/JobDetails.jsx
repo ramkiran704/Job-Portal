6866b95cc40cbd646704c5b0c4551b13d536de78
Niya006 <24cs204@mgits.ac.in>
2026-06-28 09:35:42 +0530

    Set up React Router and initial page structure

---
+++ frontend/src/pages/JobDetails.jsx
@@ -0,0 +1,9 @@
+function JobDetails() {
+    return (
+      <div>
+        <h1>Job Details Page</h1>
+      </div>
+    );
+  }
+  
+  export default JobDetails;
---
+++ frontend/src/pages/RecruiterDashboard.jsx
@@ -0,0 +1,9 @@
+function RecruiterDashboard() {
+    return (
+      <div>
+        <h1>Recruiter Dashboard</h1>
+      </div>
+    );
+  }
+  
+  export default RecruiterDashboard;
